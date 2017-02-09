a73e171d25bb99141a8d964bd97f796ef9a60e9c
budi7 <budi-purnomo@outlook.com>
2017-02-09 17:48:33 +0700

    reconstruct navbar : split topbar and navigation

--- resources/views/components/admin_navigation.blade.php
+++ resources/views/components/admin_navigation.blade.php
@@ -48,50 +48,12 @@
 			]; 
 ?>
 
-
-<!-- navbar first layer -->
-<nav class="navbar navbar-fixed-top navbar-default layer1">
-	<div class="container-fluid">
-		<!-- left section -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">KoperasiPRO</a>
-			<span class="pull-right">
-			</span>
-		</div>
-		<!-- right section -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<div class="nav navbar-nav navbar-right">
-				<a class="profile" href="#">
-					<div class="col-xs-3">
-						<img src="https://lh3.ggpht.com/tQOw8-NUQPfJdBCPdMw10ub69krfnHUbtHYD9Z3FzoEuw_PdSleXgUaZl29CSobSGg=h900" class="img-circle avatar" alt="Profile">
-					</div>
-					<div class="col-xs-9 p-r-none">
-						<p class="name">Sunarto</p>
-						<p class="role">Marketing</p>
-						<span class="pull-right dropdown">
-							<p>
-								<i class="fa fa-angle-down" aria-hidden="true"></i>
-							</p>
-						</span>
-					</div >
-				</a>
-			</div>
-		</div>
-	</div>
-</nav>
-
-<!-- second layer -->
-<nav class="navbar navbar-fixed-top layer2">
+<!-- first layer -->
+<nav class="navbar navbar-fixed-top layer1">
 	<div class="container-fluid p-l-none p-r-none">
 		<ul class="nav navbar-nav menu-list">
 			@foreach($nav as $key => $item)
-			<li>
+			<li class="@yield($key)">
 				@if(is_null($item['route']))
 				<a data-toggle="tab" href="#{{ $key }}" aria-expanded="false">
 					{{ ucWords(str_replace('_', ' ', $key)) }}
@@ -107,14 +69,14 @@
 	</div>
 </nav> 
 
-<!-- third layer -->
-<nav class="navbar navbar-fixed-top layer3">
+<!-- second layer -->
+<nav class="navbar navbar-fixed-top layer2">
 	<div class="container-fluid p-l-none p-r-none">
 		@foreach($nav as $key => $item)
 		@if(count($item['sub']) > 0)
-		<ul id="{{ $key }}" class="nav navbar-nav menu-list tab-pane fade">
+		<ul id="{{ $key }}" class="nav navbar-nav menu-list tab-pane fade @yield($key)">
 			@foreach($item['sub'] as $caption => $route)
-			<li>
+			<li class="@yield($caption)">
 				<a href="{{ $route }}">
 					{{ ucWords(str_replace('_', ' ', $caption)) }}
 				</a>
@@ -124,4 +86,4 @@
 		@endif
 		@endforeach					
 	</div>
-</nav> 
+</nav>
---
+++ resources/views/components/admin_topbar.blade.php
@@ -0,0 +1,36 @@
+<!-- topbar -->
+<nav class="navbar navbar-fixed-top navbar-default topbar">
+	<div class="container-fluid">
+		<!-- left section -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">KoperasiPRO</a>
+			<span class="pull-right">
+			</span>
+		</div>
+		<!-- right section -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="nav navbar-nav navbar-right">
+				<a class="profile" href="#">
+					<div class="col-xs-3">
+						<img src="https://lh3.ggpht.com/tQOw8-NUQPfJdBCPdMw10ub69krfnHUbtHYD9Z3FzoEuw_PdSleXgUaZl29CSobSGg=h900" class="img-circle avatar" alt="Profile">
+					</div>
+					<div class="col-xs-9 p-r-none">
+						<p class="name">Sunarto</p>
+						<p class="role">Marketing</p>
+						<span class="pull-right dropdown">
+							<p>
+								<i class="fa fa-angle-down" aria-hidden="true"></i>
+							</p>
+						</span>
+					</div >
+				</a>
+			</div>
+		</div>
+	</div>
+</nav>
