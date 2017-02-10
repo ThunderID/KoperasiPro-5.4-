cbe65fcdf43dcd93877539f252bd5073a7198358
budi7 <budi-purnomo@outlook.com>
2017-02-10 11:18:13 +0700

    re-mixed add form wizard

--- public/js/app.186e2c707859921ae77c.js
+++ public/js/app.186e2c707859921ae77c.js
@@ -151,38 +151,42 @@ __webpack_require__(13);
 /* 4 */
 /***/ (function(module, exports) {
 
-window.wizard = function (header, content, initialize) {
-	$(initialize).steps({
-		headerTag: header,
-		bodyTag: content,
-		transitionEffect: "slideLeft",
-		stepsOrientation: "vertical",
-		titleTemplate: '<span class="number">Step #index# :</span> #title#',
-		/* Labels */
-		labels: {
-			cancel: "Cancel",
-			current: "current step:",
-			pagination: "Pagination",
-			finish: "Simpan",
-			next: "SeLanjutnya",
-			previous: "Sebelumnya",
-			loading: "Loading ..."
-		},
-		/* Event */
-		onStepChanged: function onStepChanged(event, currentIndex, priorIndex) {
-			resizeJquerySteps();
-		},
-		onInit: function onInit(event, currentIndex) {
-			resizeJquerySteps();
-		},
-		onFinished: function onFinished(event, currentIndex) {
-			$('.form').submit();
-		}
-	});
-	function resizeJquerySteps() {
-		$('.wizard .content').animate({ height: $('.body.current').outerHeight() }, "slow");
+// window.wizard = function (header, content, initialize) {
+$('.wizard').steps({
+	headerTag: 'h2',
+	bodyTag: 'section',
+	transitionEffect: "slideLeft",
+	stepsOrientation: "vertical",
+	titleTemplate: '<span class="number">Step #index# :</span> #title#',
+	/* Labels */
+	labels: {
+		cancel: "Cancel",
+		current: "current step:",
+		pagination: "Pagination",
+		finish: "Simpan",
+		next: "SeLanjutnya",
+		previous: "Sebelumnya",
+		loading: "Loading ..."
+	},
+	/* Event */
+	onStepChanged: function onStepChanged(event, currentIndex, priorIndex) {
+		resizeJquerySteps();
+	},
+	onInit: function onInit(event, currentIndex) {
+		resizeJquerySteps();
+	},
+	onFinished: function onFinished(event, currentIndex) {
+		$('.form').submit();
 	}
-};
+});
+function resizeJquerySteps() {
+	$('.wizard .content').animate({ height: $('.body.current').outerHeight() }, "slow");
+}
+// }
+// $.wizard('h2', 'section', '.wizard');
+// $(document).ready(function(){
+// 	$('.wizard').wizard;
+// });
 
 /***/ }),
 /* 5 */
