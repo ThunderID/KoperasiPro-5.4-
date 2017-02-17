ecc6e875028327ea6fcc719ce2af3eb409c00e6f
chelsy <chelsymooy1108@gmail.com>
2017-02-17 16:03:27 +0700

    Repair province and city scheme

--- app/UI/CountryLists/Model/City.php
+++ app/UI/CountryLists/Model/City.php
@@ -2,12 +2,32 @@
 
 namespace App\UI\CountryLists\Model;
 
-class City
-{
-	public static function get()
-	{
-		$city 	= file_get_contents(app_path().'/UI/CountryLists/Libraries/json/cities.json');
+use Illuminate\Support\Collection;
 
-		return $city;
+class City extends Collection
+{
+	public function __construct($collection = [])
+	{
+		if(empty($collection))
+		{
+			$province   = file_get_contents(app_path().'/UI/CountryLists/Libraries/json/cities.json');
+	    
+	       	$collection = json_decode($province, true);
+		}
+
+       	parent::__construct($collection);
+	}
+
+	function withProvince()
+	{
+		$province 					= new Province;
+
+		foreach ($this->items as $key => $value) 
+		{
+			$items[$key]  				= $value;
+			$items[$key]['province']	= $province->where('province_id', $value['province_id']);
+		}
+
+		return new City($items);
 	}
 }
--- app/UI/CountryLists/Model/Province.php
+++ app/UI/CountryLists/Model/Province.php
@@ -2,13 +2,32 @@
 
 namespace App\UI\CountryLists\Model;
 
-class Province
+use Illuminate\Support\Collection;
+
+class Province extends Collection
 {
-    public static function get()
-    {
-        $city   = file_get_contents(app_path().'/UI/CountryLists/Libraries/json/provinces.json');
+	public function __construct($collection = [])
+	{
+		if(empty($collection))
+		{
+			$province   = file_get_contents(app_path().'/UI/CountryLists/Libraries/json/provinces.json');
+	    
+	       	$collection = json_decode($province, true);
+		}
 
-        return $city;
-    }
+       	parent::__construct($collection);
+	}
 
+	function withCities()
+	{
+		$city 						= new City;
+
+		foreach ($this->items as $key => $value) 
+		{
+			$items[$key]  			= $value;
+			$items[$key]['cities']	= $city->where('province_id', $value['province_id']);
+		}
+
+		return new Province($items);
+	}
 }
