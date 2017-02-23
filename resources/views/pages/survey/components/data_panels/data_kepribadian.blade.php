009cfd809bd62fa079531e68cf5e00efd447c7bd
budi7 <budi-purnomo@outlook.com>
2017-02-23 15:20:48 +0700

    restructure survey views

---
+++ resources/views/pages/survey/components/data_panels/data_kepribadian.blade.php
@@ -0,0 +1,16 @@
+<div class="row">
+	<div class="col-sm-12">
+		<h4>Data Kepribadian</h4>
+		<hr/>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-12">
+		<p>Belum ada data disimpan. <a href="#"> Tambahkan Sekarang </a></p>
+	</div>
+</div>
+
+<div class="row clearfix">&nbsp;</div>
+<div class="row clearfix">&nbsp;</div>
+<div class="row clearfix">&nbsp;</div>
