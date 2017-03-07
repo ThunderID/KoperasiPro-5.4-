d926fc14a3c406bcdf45dbf1d047d58240efd5a2
chelsy <chelsymooy1108@gmail.com>
2017-03-07 13:24:46 +0700

    minor bug about address

--- resources/views/components/helpers/forms/address.blade.php
+++ resources/views/components/helpers/forms/address.blade.php
@@ -7,7 +7,7 @@
 	<label for="">Jalan</label>
 	<div class="row">
 		<div class="col-md-8">
-			{!! Form::text('alamat[jalan]', null, ['class' => 'form-control auto-tabindex', 'placeholder' => 'Ex. Jln. Blimbing No. 8']) !!}
+			{!! Form::text('alamat[street]', null, ['class' => 'form-control auto-tabindex', 'placeholder' => 'Ex. Jln. Blimbing No. 8']) !!}
 		</div>
 		<div class="col-md-3 p-l-none">
 			<a href="#" class="btn btn-link btn-sm p-l-none p-r-none open-modal" data-toggle="modal" data-target=".modal"><i class="fa fa-search"></i> Cari Alamat yg Ada</a>
@@ -19,7 +19,7 @@
 	<label for="">Provinsi</label>
 	<div class="row">
 		<div class="col-md-5">
-			{!! Form::select('alamat[provinsi]', $param['province'], null, ['class' => 'form-control auto-tabindex select select-province', 'placeholder' => 'Pilih Provinsi', 'data-placeholder' => 'Pilih Provinsi', 'data-url' => route('cities.index')]) !!}
+			{!! Form::select('alamat[province]', $param['province'], null, ['class' => 'form-control auto-tabindex select select-province', 'placeholder' => 'Pilih Provinsi', 'data-placeholder' => 'Pilih Provinsi', 'data-url' => route('cities.index')]) !!}
 		</div>
 	</div>
 </fieldset>
@@ -28,7 +28,7 @@
 	<label for="">Kota</label>
 	<div class="row">
 		<div class="col-md-5">
-			{!! Form::select('alamat[kota]', [], null, ['class' => 'form-control auto-tabindex select select-cities', 'placeholder' => 'Pilih Kota', 'data-placeholder' => 'Pilih Kota']) !!}
+			{!! Form::select('alamat[city]', [], null, ['class' => 'form-control auto-tabindex select select-cities', 'placeholder' => 'Pilih Kota', 'data-placeholder' => 'Pilih Kota']) !!}
 		</div>
 	</div>
 </fieldset>
@@ -37,7 +37,7 @@
 	<label for="">Kodepos</label>
 	<div class="row">
 		<div class="col-md-2">
-			{!! Form::number('alamat[kode_pos]', null, ['class' => 'form-control auto-tabindex required input-kodepos', 'placeholder' => 'Kodepos']) !!}
+			{!! Form::number('alamat[zipcode]', null, ['class' => 'form-control auto-tabindex required input-kodepos', 'placeholder' => 'Kodepos']) !!}
 		</div>
 	</div>
 </fieldset>
