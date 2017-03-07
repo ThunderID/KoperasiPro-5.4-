a8577edd82a0a4afa9df9e0244e66a2402680ae0
budi7 <budi-purnomo@outlook.com>
2017-03-07 11:56:17 +0700

    fixed alertbox component

--- resources/views/components/alertbox.blade.php
+++ resources/views/components/alertbox.blade.php
@@ -4,8 +4,8 @@
 		    <div class="col-lg-12">
 		        <div class="alert alert-{{ key(Session::get('msg')) }} alert-dismissable m-b-none" style="margin-top: 0px;margin-bottom:0px">
 		            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    	            @foreach (Session::get('msg')[key(Session::get('msg'))] as $error)
-		                <p>{!! $error !!}</p>
+    	            @foreach (Session::get('msg')[key(Session::get('msg'))] as $msg)
+		                <p>{!! $msg !!}</p>
 		            @endforeach
     	            @foreach ($errors->all('<p>:message</p>') as $error)
 		                {!! $error !!}
