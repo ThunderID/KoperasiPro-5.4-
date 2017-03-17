09e7ed224c52914948dd44557d55c54b4605082f
chelsy <chelsymooy1108@gmail.com>
2017-03-17 15:52:39 +0700

    repair relationship

--- web/Queries/Credit/DaftarKredit.php
+++ web/Queries/Credit/DaftarKredit.php
@@ -67,7 +67,7 @@ class DaftarKredit
 	 */
 	public function detailed($id)
 	{
-		$model 			= $this->model->id($id)->with(['kreditur', 'jaminankendaraan', 'jaminantanahbangunan', 'jaminantanahbangunan.alamat', 'riwayat_status'])->first();
+		$model 			= $this->model->id($id)->with(['kreditur', 'jaminan_kendaraan', 'jaminan_tanah_bangunan', 'jaminan_tanah_bangunan.alamat', 'riwayat_status'])->first();
 
 		$parsed_credit 	=  $model->toArray();
 
