fdebb677c841c0da5afb14b6bb7c767feda9cf9c
Agil Mahendra <agil.mahendra@gmail.com>
2017-04-10 18:43:28 +0700

    fix error pekerjaan

--- resources/views/pages/kredit/components/form/pengajuan/pekerjaan.blade.php
+++ resources/views/pages/kredit/components/form/pengajuan/pekerjaan.blade.php
@@ -42,7 +42,7 @@
 	<label for="">Jenis Pekerjaan</label>
 	<div class="row">
 		<div class="col-md-12">
-			{!! Form::select('kreditur[pekerjaan]', $data['select_jenis_pekerjaan'], (isset($data['merk']) ? (in_array($param['data']['pekerjaan'], ['tidak_bekerja', 'karyawan_swasta', 'nelayan', 'pegawai_negeri', 'petani', 'polri', 'wiraswasta']) ? $param['data']['pekerjaan'] : 'lain_lain') : 'tidak_bekerja'), ['class' => 'form-control quick-select auto-tabindex focus select-pekerjaan', 'data-other' => 'input-jenis-pekerjaan']) !!} <br/>
+			{!! Form::select('kreditur[pekerjaan]', $data['select_jenis_pekerjaan'], (isset($param['data']['pekerjaan']) ? (in_array($param['data']['pekerjaan'], ['tidak_bekerja', 'karyawan_swasta', 'nelayan', 'pegawai_negeri', 'petani', 'polri', 'wiraswasta']) ? $param['data']['pekerjaan'] : 'lain_lain') : 'tidak_bekerja'), ['class' => 'form-control quick-select auto-tabindex focus select-pekerjaan', 'data-other' => 'input-jenis-pekerjaan']) !!} <br/>
 			{!! Form::text('kreditur[pekerjaan]', (isset($param['data']['pekerjaan']) ? $param['data']['pekerjaan'] : 'tidak_bekerja'), ['class' => 'form-control auto-tabindex m-t-sm input-jenis-pekerjaan ' . (in_array($param['data']['pekerjaan'], ['tidak_bekerja', 'karyawan_swasta', 'nelayan', 'pegawai_negeri', 'petani', 'polri', 'wiraswasta']) ? 'hidden' : (!isset($param['data']['pekerjaan']) ? 'hidden' : '')), 'placeholder' => 'Sebutkan', 'style' => 'width:40%;']) !!}
 		</div>
 	</div>
