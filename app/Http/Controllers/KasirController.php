e4618a9342cf8d2aa2195e674c2f3229f438ab3f
budi7 <budi-purnomo@outlook.com>
2017-06-03 12:36:25 +0700

    update billing store controller

--- app/Http/Controllers/KasirController.php
+++ app/Http/Controllers/KasirController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use TQueries\Kredit\DaftarKreditur;
 
+
+Header Transaksi
+use App\Domain\Kasir\Models\DetailTransaksi;
+use App\Domain\Kasir\Models\HeaderTransaksi;
+
 use Illuminate\Http\Request;
 use Exception;
 
@@ -44,7 +49,32 @@ class KasirController extends Controller
 
     public function billingStore ()
     {
+        try{
+            // init
+            $DetailTransaksi                    = new DetailTransaksi;
+            $HeaderTransaksi                    = new HeaderTransaksi;
+
+            // parase inputs
+            $input                              = Input::all();
+
+            // validate inputs
+
+            // save inputs
+
+
+        }catch (Exception $e){
+            if (is_array($e->getMessage()))
+            {
+                $this->page_attributes->msg['error']    = $e->getMessage();
+            }
+            else
+            {
+                $this->page_attributes->msg['error']    = [$e->getMessage()];
+            }
         
+            return $this->generateRedirect(route('kasir.billing'));
+        }
+
     }
 
 }
