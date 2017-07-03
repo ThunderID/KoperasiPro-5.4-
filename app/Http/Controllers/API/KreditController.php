87bc48413d1d034873449820dd0fa7f2d8d3441c
chelsymooy <chelsymooy1108@gmail.com>
2017-07-03 11:57:51 +0700

    where has issue

--- app/Http/Controllers/API/KreditController.php
+++ app/Http/Controllers/API/KreditController.php
@@ -30,7 +30,7 @@ class KreditController extends Controller
 	public function index()
 	{
 		$mobile_id 	= $this->request->input('id');
-		$data 		= Pengajuan::wherehas('melalui_mobile', function($q)use($mobile_id){$q->where('mobile_id', $mobile_id);})->get();
+		$data 		= Pengajuan::wherehas('hp', function($q)use($mobile_id){$q->where('mobile_id', $mobile_id);})->get();
 		
 		return JSend::success($data)->asArray();
 	}
