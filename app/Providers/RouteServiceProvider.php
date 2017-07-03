78d700b3126187737dd089bc548137b71d13b178
chelsymooy <chelsymooy1108@gmail.com>
2017-07-03 11:47:48 +0700

    namespace issue

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -16,8 +16,8 @@ class RouteServiceProvider extends ServiceProvider
 	 */
 	protected $namespace = 'App\Http\Controllers';
 
-	protected $api_namespace = 'App\API\Controllers';
-
+	protected $api_namespace = 'App\Http\Controllers\API';
+	
 	/**
 	 * Define your route model bindings, pattern filters, etc.
 	 *
