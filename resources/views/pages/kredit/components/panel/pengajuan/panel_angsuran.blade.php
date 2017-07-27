752deba3408f8e78008431f7cc40d92443d25fb0
Agil Mahendra <agil.mahendra@gmail.com>
2017-07-27 14:26:36 +0700

    modify show kredit
    - update panel on show kredit

--- resources/views/pages/kredit/components/panel/pengajuan/panel_angsuran.blade.php
+++ resources/views/pages/kredit/components/panel/pengajuan/panel_angsuran.blade.php
@@ -1,8 +1,8 @@
-<div class="row m-l-none m-r-none m-b-md">
+<div class="row m-b-md">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		<div data-panel="data-angsuran">
 			{{-- include component data angsuran --}}
-			ini content angsuran
+			<i class="fa fa-exclamation-triangle"></i> sorry. content underconstruction
 		</div>
 	</div>
 </div>
--- resources/views/pages/kredit/components/panel/pengajuan/panel_riwayat_status.blade.php
+++ resources/views/pages/kredit/components/panel/pengajuan/panel_riwayat_status.blade.php
@@ -1,4 +1,4 @@
-<div class="row m-l-none m-r-none m-b-md">
+<div class="row m-b-md">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		<div data-panel="data-riwayat-kredit">
 			<div class="row">
