8a0862644b5ec153d119e9f68866b67a9a6c250c
budi7 <budi-purnomo@outlook.com>
2017-08-05 12:07:56 +0700

    add foto survei on jaminan kendaraan

--- resources/views/pages/kredit/components/form/survei/jaminan_kendaraan.blade.php
+++ resources/views/pages/kredit/components/form/survei/jaminan_kendaraan.blade.php
@@ -1,4 +1,5 @@
 {!! Form::hidden('jaminan_kendaraan[id]', (isset($param['data']['id']) ? $param['data']['id'] : null)) !!}
+<h5 class="text-uppercase text-light">Data Kendaraan</h5>
 <fieldset class="form-group">
 	<label class="text-sm">Jenis Kendaraan</label>
 	<div class="row">
@@ -9,21 +10,21 @@
 	</div>
 </fieldset>
 <fieldset class="form-group">
-	<label class="text-sm">Tahun</label>
-	<div class="row">
-		<div class="col-md-2">
-			{!! Form::text('jaminan_kendaraan[tahun]', (isset($param['data']['tahun']) ? $param['data']['tahun'] : null), ['class' => 'form-control auto-tabindex  mask-year', 'placeholder' => 'Tahun', 'data-field' => 'tahun']) !!}
-		</div>
-	</div>
-</fieldset>
-<fieldset class="form-group">
 	<label class="text-sm">Merk</label>
 	<div class="row">
 		<div class="col-md-4">
 			{!! Form::select('jaminan_kendaraan[merk]', $page_datas->select_merk_kendaraan, 
 				(isset($param['data']['merk']) ? (in_array($param['data']['merk'], ['daihatsu', 'honda', 'isuzu', 'kawasaki', 'kia', 'mitsubishi', 'nissan', 'suzuki', 'toyota', 'yamaha']) ? $param['data']['merk'] : 'lain_lain') : 'daihatsu'), 
-				['class' => 'form-control auto-tabindex quick-select select', 'placeholder' => 'Merk Kendaraan', 'data-other' => 'input-merk-kendaraan']) !!} <br/>
+				['class' => 'form-control auto-tabindex quick-select select', 'placeholder' => 'Merk Kendaraan', 'data-other' => 'input-merk-kendaraan']) !!}
 			{{-- {!! Form::text('jaminan_kendaraan[merk]', (isset($param['data']['merk']) ? $param['data']['merk'] : 'daihatsu'), ['class' => 'form-control auto-tabindex m-t-sm input-merk-kendaraan input-kendaraan ' . (isset($param['data']['merk']) && (in_array($param['data']['merk'], ['daihatsu', 'honda', 'isuzu', 'kawasaki', 'kia', 'mitsubishi', 'nissan', 'suzuki', 'toyota', 'yamaha']) ? 'hidden' : (!isset($param['data']['merk']) ? 'hidden' : ''))), 'placeholder' => 'Sebutkan', 'style' => 'width:40%;']) !!} --}}
+		</div>
+	</div>
+</fieldset>
+<fieldset class="form-group">
+	<label class="text-sm">Tahun</label>
+	<div class="row">
+		<div class="col-md-2">
+			{!! Form::text('jaminan_kendaraan[tahun]', (isset($param['data']['tahun']) ? $param['data']['tahun'] : null), ['class' => 'form-control auto-tabindex  mask-year', 'placeholder' => 'Tahun', 'data-field' => 'tahun']) !!}
 		</div>
 	</div>
 </fieldset>
@@ -41,37 +42,8 @@
 					'lain_lain'		=> 'Lainnya',
 				], 
 				(isset($param['data']['warna']) ? (in_array($param['data']['warna'], ['biru', 'hijau', 'hitam', 'merah', 'merah_muda', 'orange']) ? $param['data']['warna'] : 'lain_lain') : 'biru'), 
-				['class' => 'form-control auto-tabindex quick-select select', 'placeholder' => 'Merk Kendaraan', 'data-other' => 'input-merk-kendaraan']) !!} <br/>
+				['class' => 'form-control auto-tabindex quick-select select', 'placeholder' => 'Merk Kendaraan', 'data-other' => 'input-merk-kendaraan']) !!}
 			{{-- {!! Form::text('jaminan_kendaraan[warna]', (isset($param['data']['warna']) ? $param['data']['warna'] : 'biru'), ['class' => 'form-control auto-tabindex m-t-sm input-merk-kendaraan input-kendaraan ' . (isset($param['data']['warna']) && (in_array($param['data']['warna'], ['biru', 'hijau', 'hitam', 'merah', 'merah_muda', 'orange'])) ? 'hidden' : (!isset($param['data']['warna']) ? 'hidden' : '')), 'placeholder' => 'Sebutkan', 'style' => 'width:40%;']) !!} --}}
-		</div>
-	</div>
-</fieldset>
-<fieldset class="form-group">
-	<label class="text-sm">Atas Nama</label>
-	<div class="row">
-		<div class="col-md-7">
-			{!! Form::text('jaminan_kendaraan[atas_nama]', (isset($param['data']['atas_nama']) ? $param['data']['atas_nama'] : null), ['class' => 'form-control auto-tabindex input-kendaraan', 'placeholder' => 'Atas Nama', 'data-field' => 'atas_nama']) !!}
-		</div>
-	</div>
-</fieldset>
-
-
-@include('components.helpers.forms.address', [
-	'param'		=> [
-		'prefix'	=> 'jaminan_kendaraan',
-		'data'		=> isset($param['data']['alamat']) ? $param['data']['alamat'] : null,
-	],
-	'data'		=> ['provinsi' 	=> $page_datas->provinsi],
-	'settings'	=> [
-		'class'		=> 'input-kendaraan'
-	]
-])
-
-<fieldset class="form-group">
-	<label class="text-sm">Nomor BPKB</label>
-	<div class="row">
-		<div class="col-md-4">
-			{!! Form::text('jaminan_kendaraan[nomor_bpkb]', (isset($param['data']['nomor_bpkb']) ? $param['data']['nomor_bpkb'] : null), ['class' => 'form-control auto-tabindex input-kendaraan', 'placeholder' => 'Nomor BPKB', 'data-field' => 'nomor_bpkb']) !!}
 		</div>
 	</div>
 </fieldset>
@@ -83,7 +55,7 @@
 		</div>
 	</div>
 </fieldset>
-<fieldset class="form-group">
+<fieldset class="form-group p-b-sm">
 	<label class="text-sm">Nomor Rangka</label>
 	<div class="row">
 		<div class="col-md-4">
@@ -91,6 +63,25 @@
 		</div>
 	</div>
 </fieldset>
+<h5 class="text-uppercase text-light">Data Legalitas Kendaraan</h5>
+<fieldset class="form-group">
+	<label class="text-sm">Atas Nama</label>
+	<div class="row">
+		<div class="col-md-6">
+			{!! Form::text('jaminan_kendaraan[atas_nama]', (isset($param['data']['atas_nama']) ? $param['data']['atas_nama'] : null), ['class' => 'form-control auto-tabindex input-kendaraan', 'placeholder' => 'Atas Nama', 'data-field' => 'atas_nama']) !!}
+		</div>
+	</div>
+</fieldset>
+
+<fieldset class="form-group">
+	<label class="text-sm">Nomor BPKB</label>
+	<div class="row">
+		<div class="col-md-4">
+			{!! Form::text('jaminan_kendaraan[nomor_bpkb]', (isset($param['data']['nomor_bpkb']) ? $param['data']['nomor_bpkb'] : null), ['class' => 'form-control auto-tabindex input-kendaraan', 'placeholder' => 'Nomor BPKB', 'data-field' => 'nomor_bpkb']) !!}
+		</div>
+	</div>
+</fieldset>
+
 <fieldset class="form-group">
 	<label class="text-sm">Nomor Polisi</label>
 	<div class="row">
@@ -99,7 +90,7 @@
 		</div>
 	</div>
 </fieldset>
-<fieldset class="form-group">
+<fieldset class="form-group p-b-sm">
 	<label class="text-sm">Masa Berlaku STNK</label>
 	<div class="row">
 		<div class="col-md-5">
@@ -108,6 +99,20 @@
 		</div>
 	</div>
 </fieldset>
+
+@include('components.helpers.forms.address', [
+	'param'		=> [
+		'prefix'	=> 'jaminan_kendaraan',
+		'data'		=> isset($param['data']['alamat']) ? $param['data']['alamat'] : null,
+	],
+	'data'		=> ['provinsi' 	=> $page_datas->provinsi],
+	'settings'	=> [
+		'class'		=> 'input-kendaraan'
+	],
+	'title'		=> 'Data Alamat Sesuai BPKB'
+])
+
+<h5 class="text-uppercase text-light p-t-sm">Data Penunjang</h5>
 <fieldset class="form-group">
 	<label class="text-sm">Harga Taksasi</label>
 	<div class="row">
@@ -127,8 +132,18 @@
 <fieldset class="form-group">
 	<label class="text-sm">Fungsi Sehari-hari</label>
 	<div class="row">
-		<div class="col-md-5">
+		<div class="col-md-6">
 			{!! Form::text('jaminan_kendaraan[fungsi_sehari_hari]', (isset($param['data']['fungsi_sehari_hari']) ? $param['data']['fungsi_sehari_hari'] : null), ['class' => 'form-control auto-tabindex', 'placeholder' => '']) !!}
+		</div>
+	</div>
+</fieldset>
+<fieldset class="form-group m-b-sm">
+	<label for="jaminan_kendaraan[foto]" class="text-sm">Foto Jaminan</label>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="thunder-imgUploader" thunder-imgUploader-subtitle = "* File maksimum 200kb" thunder-imageUploader-store-url="{{ route('helper.gambar.store') }}" thunder-imageUploader-remove-url="{{ route('helper.gambar.destroy') }}">
+	            <input id="jaminan_kendaraan[foto]" name="jaminan_kendaraan[foto]" type="file" multiple accept='image/jpeg'/>
+	        </div>		
 		</div>
 	</div>
 </fieldset>
